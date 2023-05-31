ce68ec23c74fe447f05c093e9ab46bc833f8a4e2
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-05-31 16:02:01 -0300

    cxl btn

--- resources/js/Pages/Projects/Partials/NewProjectForm.vue
+++ resources/js/Pages/Projects/Partials/NewProjectForm.vue
@@ -45,12 +45,14 @@ const form = useForm({
                 <InputError class="mt-2" :message="form.errors.description" />
             </div>
 
-            <div class="flex items-center gap-4">
+            <div class="flex items-center justify-between gap-4">
                 <PrimaryButton :disabled="form.processing">Save</PrimaryButton>
 
                 <Transition enter-from-class="opacity-0" leave-to-class="opacity-0" class="transition ease-in-out">
                     <p v-if="form.recentlySuccessful" class="text-sm text-gray-600 dark:text-gray-400">Saved.</p>
                 </Transition>
+
+                <Link :href="route('projects')" class="inline-flex items-center px-4 py-2 bg-red-800 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-red-800 uppercase tracking-widest hover:bg-red-700 dark:hover:bg-white focus:bg-red-700 dark:focus:bg-white active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150">Cancel</Link>
             </div>
         </form>
     </section>
