2f1a90f2fb130e81c287a2ad2e20f27f40087304
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-05 15:51:02 -0300

    show user tasks

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Task;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -34,9 +35,80 @@ class UserController extends Controller
      */
     public function show(User $user): Response
     {
+        // all the tasks below have to obey one the following rules in order to be displayed to the current user:
+        // 1. the task has to be assigned to the current user
+        // 2. the task has to be owned by the current user
+        // 3. the task has to be assigned to a team that the current user is a member of
+        // get tasks for the selected user that don't have a project_id
+        $myTasks = Task::where('user_id', $user->_id)
+            ->whereNull('project_id')
+            ->where(function ($query) {
+                $query->where('assigned_to', auth()->id())
+                    ->orWhere('team', 'LIKE', '%' . auth()->id() . '%');
+            })
+            ->with('user')
+            ->get();
+        // get tasks for the selected user that have a project_id, eager load the project and group them by project
+        $myProjectTasks = Task::where('user_id', $user->_id)
+            ->whereNotNull('project_id')
+            ->where(function ($query) {
+                $query->where('assigned_to', auth()->id())
+                    ->orWhere('team', 'LIKE', '%' . auth()->id() . '%');
+            })
+            ->with('project', 'user')
+            ->get()
+            ->groupBy('project_id');
+        // get the assigned tasks for the selected user that don't have a project_id
+        $assignedTasks = Task::where('assigned_to', $user->_id)
+            ->whereNull('project_id')
+            ->where(function ($query) {
+                $query->where('user_id', auth()->id())
+                    ->orWhere('team', 'LIKE', '%' . auth()->id() . '%');
+            })
+            ->with('user')
+            ->get();
+        // get the assigned tasks for the selected user that have a project_id, eager load the project and group them by project
+        $assignedProjectTasks = Task::where('assigned_to', $user->_id)
+            ->whereNotNull('project_id')
+            ->where(function ($query) {
+                $query->where('user_id', auth()->id())
+                    ->orWhere('team', 'LIKE', '%' . auth()->id() . '%');
+            })
+            ->with('project', 'user')
+            ->get()
+            ->groupBy('project_id');
+        // get team tasks for the selected user that don't have aproject_id, team is an array of user_ids, so this user_id must be inside this array
+        $teamTasks = Task::where('team', 'LIKE', '%' . auth()->id() . '%')
+            ->whereNull('project_id')
+            ->where(function ($query) {
+                $query->where('user_id', auth()->id())
+                    ->orWhere('assigned_to', auth()->id());
+            })
+            ->with('user')
+            ->get();
+        // get team tasks for the selected user that have a project_id, eager load the project and group them by project, team is an array of user_ids, so this user_id must be inside this array
+        $teamProjectTasks = Task::where('team', 'LIKE', '%' . auth()->id() . '%')
+            ->whereNotNull('project_id')
+            ->where(function ($query) {
+                $query->where('user_id', auth()->id())
+                    ->orWhere('assigned_to', auth()->id());
+            })
+            ->with('project', 'user')
+            ->get()
+            ->groupBy('project_id');
+        // get all the users details grouped by _id
+        $users = User::all()->keyBy('_id');        
+
         // return the users show view with the user
         return Inertia::render('Users/Show', [
             'user' => $user,
+            'myTasks' => $myTasks,
+            'myProjectTasks' => $myProjectTasks,
+            'assignedTasks' => $assignedTasks,
+            'assignedProjectTasks' => $assignedProjectTasks,
+            'teamTasks' => $teamTasks,
+            'teamProjectTasks' => $teamProjectTasks,
+            'users' => $users,
         ]);
     }
 }
--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -23,87 +23,87 @@ const activeTab = ref('myTasks');
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Tasks</h2>
         </template>
 
-            <div class="bg-white dark:bg-gray-800 overflow-hidden">
-                <div class="flex bg-gray-50 dark:bg-gray-950">
-                    <div
-                        :class="{
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
-                                activeTab === 'myTasks',
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
-                                activeTab !== 'myTasks'
-                        }"
-                        @click="activeTab = 'myTasks'"
-                    >
-                        My Tasks
+        <div class="bg-white dark:bg-gray-800 overflow-hidden">
+            <div class="flex bg-gray-50 dark:bg-gray-950">
+                <div
+                    :class="{
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
+                            activeTab === 'myTasks',
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
+                            activeTab !== 'myTasks'
+                    }"
+                    @click="activeTab = 'myTasks'"
+                >
+                    My Tasks
+                </div>
+                <div
+                    :class="{
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
+                            activeTab === 'assignedTasks',
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
+                            activeTab !== 'assignedTasks'
+                    }"
+                    @click="activeTab = 'assignedTasks'"
+                >
+                    Assigned Tasks
+                </div>
+                <div
+                    :class="{
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
+                            activeTab === 'teamTasks',
+                        'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
+                            activeTab !== 'teamTasks'
+                    }"
+                    @click="activeTab = 'teamTasks'"
+                >
+                    Team Tasks
+                </div>
+            </div>
+            <div class="p-4">
+                <div v-if="activeTab === 'myTasks'">
+                    <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div class="p-2">
+                            <ProjectTask :project="myTasks"/>
+                        </div>
                     </div>
-                    <div
-                        :class="{
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
-                                activeTab === 'assignedTasks',
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
-                                activeTab !== 'assignedTasks'
-                        }"
-                        @click="activeTab = 'assignedTasks'"
-                    >
-                        Assigned Tasks
-                    </div>
-                    <div
-                        :class="{
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-indigo-400 dark:border-indigo-600 text-indigo-800 dark:text-indigo-300 bg-indigo-50 dark:bg-indigo-900/50 focus:outline-none focus:text-indigo-800 dark:focus:text-indigo-200 focus:bg-indigo-100 dark:focus:bg-indigo-900 focus:border-indigo-700 dark:focus:border-indigo-300 transition duration-150 ease-in-out':
-                                activeTab === 'teamTasks',
-                            'cursor-pointer text-base font-medium p-2 text-center text-gray-600 border-b-2 border-gray-50 dark:border-gray-950':
-                                activeTab !== 'teamTasks'
-                        }"
-                        @click="activeTab = 'teamTasks'"
-                    >
-                        Team Tasks
+                    <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div v-for="(project, project_id) in myProjectTasks" :key="project_id" class="p-2">
+                            <ProjectTask :project="project"/>
+                        </div>
                     </div>
                 </div>
-                <div class="p-4">
-                    <div v-if="activeTab === 'myTasks'">
-                        <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div class="p-2">
-                                <ProjectTask :project="myTasks"/>
-                            </div>
-                        </div>
-                        <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div v-for="(project, project_id) in myProjectTasks" :key="project_id" class="p-2">
-                                <ProjectTask :project="project"/>
-                            </div>
+                <div v-else-if="activeTab === 'assignedTasks'">
+                    <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div class="p-2">
+                            <ProjectTask :project="assignedTasks"/>
                         </div>
                     </div>
-                    <div v-else-if="activeTab === 'assignedTasks'">
-                        <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div class="p-2">
-                                <ProjectTask :project="assignedTasks"/>
-                            </div>
-                        </div>
-                        <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div v-for="(project, project_id) in assignedProjectTasks" :key="project_id" class="p-2">
-                                <ProjectTask :project="project"/>
-                            </div>
+                    <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div v-for="(project, project_id) in assignedProjectTasks" :key="project_id" class="p-2">
+                            <ProjectTask :project="project"/>
                         </div>
                     </div>
-                    <div v-else-if="activeTab === 'teamTasks'">
-                        <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div class="p-2">
-                                <ProjectTask :project="teamTasks"/>
-                            </div>
+                </div>
+                <div v-else-if="activeTab === 'teamTasks'">
+                    <h2 class="text-lg font-semibold mb-2">Free Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div class="p-2">
+                            <ProjectTask :project="teamTasks"/>
                         </div>
-                        <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
-                        <div class="flex flex-col gap-2">
-                            <div v-for="(project, project_id) in teamProjectTasks" :key="project_id" class="p-2">
-                                <ProjectTask :project="project"/>
-                            </div>
+                    </div>
+                    <h2 class="text-lg font-semibold mb-2">Project Tasks</h2>
+                    <div class="flex flex-col gap-2">
+                        <div v-for="(project, project_id) in teamProjectTasks" :key="project_id" class="p-2">
+                            <ProjectTask :project="project"/>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
 
     </AuthenticatedLayout>
 </template>
