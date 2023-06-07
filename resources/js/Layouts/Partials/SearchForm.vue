338b9e0118fafb994649b1ac18d531072fc3c2ec
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-07 18:11:36 -0300

    added searchform to layout

---
+++ resources/js/Layouts/Partials/SearchForm.vue
@@ -0,0 +1,20 @@
+<script setup>
+import TextInput from '@/Components/TextInput.vue';
+import { useForm } from '@inertiajs/vue3';
+
+const props = defineProps({
+
+});
+
+const form = useForm({
+    search: '',
+});
+</script>
+
+<template>
+    <form @submit.prevent="form.get(route('search.show'))">
+        <div class="flex items-center justify-between px-4">
+            <TextInput type="search" class="mt-1 block w-full" v-model="form.search" required placeholder="Task, project or user"/>
+        </div>
+    </form>
+</template>
