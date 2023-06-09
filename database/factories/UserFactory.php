4f970b3839ec4cb5f2142c3f26a9e7f1c39ef86c
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-09 14:11:32 -0300

    default password for factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$KE/fRGptgH.tHs/iV54aJeWGsVkaZPTyBleLOfwVQ8MhHJTooFh0m', // password
+            'password' => '$2y$10$KE/fRGptgH.tHs/iV54aJeWGsVkaZPTyBleLOfwVQ8MhHJTooFh0m', // 123456123456
             'remember_token' => Str::random(10),
             'starred_projects' => [],
             'starred_users' => [],
