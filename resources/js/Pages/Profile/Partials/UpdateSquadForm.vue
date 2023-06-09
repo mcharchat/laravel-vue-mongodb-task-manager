4f48453c36133f65d685814def7994d5db00c2a5
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-09 15:48:18 -0300

    on the future will be possible to uodate the squad

---
+++ resources/js/Pages/Profile/Partials/UpdateSquadForm.vue
@@ -0,0 +1,33 @@
+<script setup>
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import TextInput from '@/Components/TextInput.vue';
+import { useForm, usePage } from '@inertiajs/vue3';
+
+const props = defineProps({
+});
+
+const user = usePage().props.auth.user;
+
+const form = useForm({
+    squad_id: user.squad_id,
+});
+</script>
+
+<template>
+    <section>
+        <header>
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">Squad Information</h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                Find below your squad ID.
+            </p>
+        </header>
+
+        <form @submit.prevent="form.patch(route('profile.update'))" class="mt-6 space-y-6">
+            <div>
+                <InputLabel for="squad_id" :value="`Squad ID: ${form.squad_id}`" />
+            </div>
+        </form>
+    </section>
+</template>
