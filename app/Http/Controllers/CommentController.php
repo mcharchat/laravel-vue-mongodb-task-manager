19627a910cac3224cf3ae6bc7a21ab1d1fdc8c55
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-14 13:44:29 -0300

    comments backend

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comment;
+use App\Models\Task;
 use App\Http\Requests\StoreCommentRequest;
 use App\Http\Requests\UpdateCommentRequest;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
 
 class CommentController extends Controller
 {
@@ -34,9 +37,27 @@ class CommentController extends Controller
      * @param  \App\Http\Requests\StoreCommentRequest  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(StoreCommentRequest $request)
+    public function store(StoreCommentRequest $request) : RedirectResponse
     {
-        //
+        //validate the incoming request using our StoreCommentRequest
+        $validated = $request->validated();
+        // set the user_id to the current user id
+        $validated['user_id'] = auth()->id();
+        // add the squad_id to the validated data
+        $validated['squad_id'] = auth()->user()->squad_id;
+        // create a new task with the validated data
+        Comment::create($validated);
+        // get the task that the comment belongs to, with all of its comments
+        $task = Task::find($validated['task_id'])->load('comments');
+        $message = [
+            'task_id' => $validated['task_id'], 
+            'comments' => $task->comments,
+        ];
+
+        // fire the CommentEvent event with the squad_id and the validated data
+        event(new CommentEvent(auth()->user()->squad_id, $message, 'create'));
+        // return a redirect to the tasks index
+        return Redirect::route('tasks', [], 303);
     }
 
     /**
--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -13,6 +13,20 @@ class StoreCommentRequest extends FormRequest
      */
     public function authorize()
     {
+        // there are three people who can authorize a request, if the comment's task is private:
+        // 1. the user who created the task
+        // 2. the user who is assigned to the task
+        // 3. one of the team members of the task
+        // if the task is public, anyone can comment
+
+        $user = $this->user();
+        $task = $this->route('task');
+        $owner = $task->user_id;
+        $assignee = $task->assigned_to ?? null;
+        $team = $task->team;
+        if ($user->id === $owner || $user->id === $assignee || in_array($user->id, $team) || $task->public === true) {
+            return true;
+        }
         return false;
     }
 
@@ -24,7 +38,8 @@ class StoreCommentRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'task_id' => 'required|exists:tasks,_id',
+            'body' => 'required|string'
         ];
     }
 }
