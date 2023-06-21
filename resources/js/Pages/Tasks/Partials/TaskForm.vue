05adaecc815629a5c1b0c0066fec19821e9a4b75
mcharchat <73533552+mcharchat@users.noreply.github.com>
2023-06-21 00:05:52 -0300

    delete if existent

--- resources/js/Pages/Tasks/Partials/TaskForm.vue
+++ resources/js/Pages/Tasks/Partials/TaskForm.vue
@@ -243,6 +243,7 @@
                     <p v-if="form.recentlySuccessful" class="text-sm text-gray-600 dark:text-gray-400">Saved.</p>
                 </Transition>
                 <span @click.prevent="deleteTask()" 
+                    v-if="task?.project_id"
                     class="cursor-pointer inline-flex items-center px-4 py-2 bg-red-800 dark:bg-red-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-red-800 uppercase tracking-widest hover:bg-red-700 dark:hover:bg-white focus:bg-red-700 dark:focus:bg-white active:bg-red-900 dark:active:bg-red-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-red-800 transition ease-in-out duration-150">
                 Delete</span>
                 <span @click.prevent="closeModal()" 
